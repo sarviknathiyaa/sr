b90506807d267dd4f9d2d42a592c07a6db97e79c
sarviknathiyaa <81229718+sarviknathiyaa@users.noreply.github.com>
2021-09-07 15:07:33 +0530

    Add files via upload

---
+++ hj.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import random
+oot=Tk()
+root.title("list")
+root.geometry("400x400")
+
+random_number_list=Label(root)
+random_number_sorted_list=Label(root)
+def randomlist():
+randomlist=random.sample(range(10,30),5)
+random_number_list["text"]="random list:"+ str(randomlist)
+randomlist.sort()
+random_number_sorted_list["text"]="sorted random number:"+ str(randomlist)
+btn=Button(root,text="generate random list",command=randomlist)
+btn.place(relx=0.5,rely=0.4,anchor=CENTER)
+random_number_list.place(relx=0.5,rely=0.5,anchor=CENTER)
+random_number_sorted_list.place(relx=0.5,rely=0.6,anchor=CENTER
+
